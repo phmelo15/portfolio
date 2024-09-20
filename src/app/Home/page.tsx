12eae1e995a6e87cd1e82c68b77807ab72723f10
Pedro <phmelo15@gmail.com>
2024-09-20 11:14:17 -0300

    removing import

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,9 +1,6 @@
-import React from "react";
-import Header from "../../components/Header/Header";
 import AboutMe from "@/components/AboutMe/AboutMe";
-import Projects from "@/components/Projects/Projects";
 import Experience from "@/components/Experience/Experience";
-import Footer from "@/components/Footer/Footer";
+import Projects from "@/components/Projects/Projects";
 import { styles } from "./styles";
 
 export default function page() {
